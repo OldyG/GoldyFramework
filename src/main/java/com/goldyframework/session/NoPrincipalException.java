95ccf448bd627ceab0110e837788d8f0f6432e00
jeonghyun.kum <jeonghyun.kum@formalworks.com>
2017-07-13 17:13:15 +0900

    ColumnMapping 구현방법 변경

---
+++ src/main/java/com/goldyframework/session/NoPrincipalException.java
@@ -0,0 +1,31 @@
+/**
+ * FileName : {@link NoPrincipalException}.java
+ * Created : 2017. 4. 10.
+ * Author : jeong
+ * Summary :
+ * Copyright (C) 2017 Goldy Project. All rights reserved.
+ * 이 문서의 모든 저작권 및 지적 재산권은 Goldy Project에게 있습니다.
+ * 이 문서의 어떠한 부분도 허가 없이 복제 또는 수정 하거나, 전송할 수 없습니다.
+ */
+package com.goldyframework.session;
+
+public class NoPrincipalException extends Exception {
+	
+	/**
+	 * Serializable UID
+	 *
+	 * @author jeong
+	 * @since 2017. 5. 22. 오후 9:20:10
+	 */
+	private static final long serialVersionUID = 4613377314762564030L;
+	
+	/**
+	 * {@link NoPrincipalException} 클래스의 새 인스턴스를 초기화 합니다.
+	 *
+	 * @author jeong
+	 * @since 2017. 4. 15. 오후 4:44:55
+	 */
+	public NoPrincipalException() {
+		super();
+	}
+}

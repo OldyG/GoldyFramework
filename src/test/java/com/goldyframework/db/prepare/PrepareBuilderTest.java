3e5e452be529d8543b16cc8fa5f775d4297e4034
jeonghyun.kum <jeonghyun.kum@formalworks.com>
2017-07-13 09:56:23 +0900

    잘못된 테스트케이스 수정

--- src/test/java/com/goldyframework/db/prepare/PrepareBuilderTest.java
+++ src/test/java/com/goldyframework/db/prepare/PrepareBuilderTest.java
@@ -25,72 +25,75 @@ import com.goldyframework.db.prepare.statement.update.UpdatePrepare;
  */
 @SuppressWarnings("nls")
 public class PrepareBuilderTest {
-	
+
 	@Test
 	public void testDelete() {
-		
+
 		final DeletePrepare delete = PrepareBuilder
-			.delete("TEST_DELETE")
-			.where("column1", Comparison.GREATER_EQUAL, 1)
-			.where("column2", Comparison.EQUAL, "true")
-			.build();
-		
+				.delete("TEST_DELETE")
+				.where("column1", Comparison.GREATER_EQUAL, 1)
+				.where("column2", Comparison.EQUAL, "true")
+				.build();
+
 		Assert.assertEquals("", "DELETE FROM "
-			+ "TEST_DELETE "
-			+ "WHERE TEST_DELETE.column1 >= ?, "
-			+ "TEST_DELETE.column2 = ?",
-			delete.toPrepareSql());
+				+ "TEST_DELETE "
+				+ "WHERE TEST_DELETE.column1 >= ?, "
+				+ "TEST_DELETE.column2 = ?",
+				delete.toPrepareSql());
 		final List<Object> args = new ArrayList<>(delete.getArgs());
 		Assert.assertEquals("", 1, args.get(0));
 		Assert.assertEquals("", "true", args.get(1));
 	}
-	
+
 	@Test
 	public void testSelect() {
-		
+
 		final SelectPrepare select = PrepareBuilder
-			.select("TEST_SELECT")
-			.column("A")
-			.column("B")
-			.column("C")
-			.where("F", Comparison.EQUAL, "QQ")
-			.where("G", Comparison.GREATER, 3)
-			.build();
+				.select("TEST_SELECT")
+				.column("A")
+				.column("B")
+				.column("C")
+				.where("F", Comparison.EQUAL, "QQ")
+				.where("G", Comparison.GREATER, 3)
+				.build();
 		Assert.assertEquals("",
-			"SELECT TEST_SELECT.A, TEST_SELECT.B, TEST_SELECT.C "
-				+ "FROM TEST_SELECT "
-				+ "WHERE TEST_SELECT.F = ?, TEST_SELECT.G > ?",
-			select.toPrepareSql());
-		
+				"SELECT TEST_SELECT.A, TEST_SELECT.B, TEST_SELECT.C "
+						+ "FROM TEST_SELECT "
+						+ "WHERE TEST_SELECT.F = ? AND TEST_SELECT.G > ?",
+				select.toPrepareSql());
+
+		System.out.println(select.toPrepareSql());
+		System.out.println(select.getArgs());
+
 		final List<Object> args = new ArrayList<>(select.getArgs());
 		Assert.assertEquals("", "QQ", args.get(0));
 		Assert.assertEquals("", 3, args.get(1));
 	}
-	
+
 	/**
 	 * Test method for {@link com.goldyframework.db.prepare.PrepareBuilder#PrepareBuilder()}.
 	 */
 	@Test
 	public void testUpdate() {
-		
+
 		final UpdatePrepare update = PrepareBuilder
-			.update("TEST_UPDATE")
-			.assign("name", "test")
-			.assign("name2", "test2")
-			.where("column1", Comparison.GREATER_EQUAL, 1)
-			.where("column2", Comparison.EQUAL, "true")
-			.build();
-		
+				.update("TEST_UPDATE")
+				.assign("name", "test")
+				.assign("name2", "test2")
+				.where("column1", Comparison.GREATER_EQUAL, 1)
+				.where("column2", Comparison.EQUAL, "true")
+				.build();
+
 		Assert.assertEquals("",
-			"UPDATE TEST_UPDATE "
-				+ "SET TEST_UPDATE.name = ?, TEST_UPDATE.name2 = ? "
-				+ "WHERE TEST_UPDATE.column1 >= ?, TEST_UPDATE.column2 = ?",
-			update.toPrepareSql());
+				"UPDATE TEST_UPDATE "
+						+ "SET TEST_UPDATE.name = ?, TEST_UPDATE.name2 = ? "
+						+ "WHERE TEST_UPDATE.column1 >= ?, TEST_UPDATE.column2 = ?",
+				update.toPrepareSql());
 		final List<Object> args = new ArrayList<>(update.getArgs());
 		Assert.assertEquals("", "test", args.get(0));
 		Assert.assertEquals("", "test2", args.get(1));
 		Assert.assertEquals("", 1, args.get(2));
 		Assert.assertEquals("", "true", args.get(3));
 	}
-	
+
 }

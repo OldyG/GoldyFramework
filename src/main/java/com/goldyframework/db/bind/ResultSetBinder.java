81ac95566338f66c6f30e36ae7bf7bc23dfd1dee
jeonghyun.kum <jeonghyun.kum@formalworks.com>
2018-01-26 10:45:03 +0900

    저작권 내용 업데이트

--- src/main/java/com/goldyframework/db/bind/ResultSetBinder.java
+++ src/main/java/com/goldyframework/db/bind/ResultSetBinder.java
@@ -3,8 +3,8 @@
  * Created : 2017. 7. 1. 오후 5:00:07
  * Author : jeong
  * Summary :
- * Copyright (C) 2017 Goldy Project. All rights reserved.
- * 이 문서의 모든 저작권 및 지적 재산권은 (주)Goldy Project에게 있습니다.
+ * Copyright (C) 2018 Formal Works Inc. All rights reserved.
+ * 이 문서의 모든 저작권 및 지적 재산권은 (주)포멀웍스에게 있습니다.
  * 이 문서의 어떠한 부분도 허가 없이 복제 또는 수정 하거나, 전송할 수 없습니다.
  */
 package com.goldyframework.db.bind;
--- src/test/java/com/goldyframework/db/cache/IntegerSetCacheTest.java
+++ src/test/java/com/goldyframework/db/cache/IntegerSetCacheTest.java
@@ -3,13 +3,13 @@
  * Created : 2018. 1. 13. 오후 8:19:06
  * Author : jeong
  * Summary :
- * Copyright (C) 2017 Goldy Project. All rights reserved.
- * 이 문서의 모든 저작권 및 지적 재산권은 (주)Goldy Project에게 있습니다.
+ * Copyright (C) 2018 Formal Works Inc. All rights reserved.
+ * 이 문서의 모든 저작권 및 지적 재산권은 (주)포멀웍스에게 있습니다.
  * 이 문서의 어떠한 부분도 허가 없이 복제 또는 수정 하거나, 전송할 수 없습니다.
  */
 package com.goldyframework.db.cache;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,57 +18,62 @@ import org.junit.Test;
  * @author 2018. 1. 13. 오후 8:19:06 jeong
  */
 public class IntegerSetCacheTest {
-
+	
 	@Test
 	public void testAdd() {
-
+		
 		final IntegerSetCache target = new IntegerSetCache("3;1;2;");
 		target.add(3);
-
+		
 		Assert.assertEquals(target.size(), 3);
 		Assert.assertTrue(target.contains(1));
 		Assert.assertTrue(target.contains(2));
 		Assert.assertTrue(target.contains(3));
 	}
-
+	
 	@Test
 	public void testAdd2() {
-
+		
 		final IntegerSetCache target = new IntegerSetCache("3;1;2;");
-		target.add(List.of(1, 2, 4));
-
+		final ArrayList<Integer> arrayList = new ArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(4);
+		
+		target.add(arrayList);
+		
 		Assert.assertEquals(target.size(), 4);
 		Assert.assertTrue(target.contains(1));
 		Assert.assertTrue(target.contains(2));
 		Assert.assertTrue(target.contains(3));
 		Assert.assertTrue(target.contains(4));
-
+		
 	}
-
+	
 	/**
 	 * Test method for {@link com.goldyframework.db.cache.IntegerSetCache#toCacheString()}.
 	 */
 	@Test
 	public void testToCacheString() {
-
+		
 		final IntegerSetCache target = new IntegerSetCache("3;1;2;");
-
+		
 		final String cacheString = target.toCacheString();
 		Assert.assertEquals(cacheString, "1;2;3;");
-
+		
 	}
-
+	
 	/**
 	 * Test method for {@link com.goldyframework.db.cache.IntegerSetCache#toList()}.
 	 */
 	@Test
 	public void testToList() {
-
+		
 		final IntegerSetCache target = new IntegerSetCache("3;1;2;");
-
+		
 		Assert.assertTrue(target.contains(1));
 		Assert.assertTrue(target.contains(2));
 		Assert.assertTrue(target.contains(3));
 	}
-
+	
 }
